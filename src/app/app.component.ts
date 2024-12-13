630785dd6fa197b17f681d6a712fb778c32633c3
Adrian Jose Diaz Guilamo <ajdiaz@ADRIANDIAZ>
2024-12-13 18:21:06 -0400

    minor changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
 	selector: 'app-root',
@@ -18,6 +19,7 @@ import { MatListModule } from '@angular/material/list';
 		MatIconModule,
 		MatButtonModule,
 		MatListModule,
+		MatMenuModule,
 	],
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.scss',
