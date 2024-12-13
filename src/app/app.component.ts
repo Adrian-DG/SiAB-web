dae10c95c26c5030676e77db0bf31b9f54d8c9ad
Adrian Jose Diaz Guilamo <ajdiaz@ADRIANDIAZ>
2024-12-13 18:04:44 -0400

    UI minor changes (toolbar & sidenav)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { IUrlOption } from './Shared/Models/IUrlOption.model';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: 'app-root',
+	standalone: true,
+	imports: [
+		RouterOutlet,
+		RouterModule,
+		MatSidenavModule,
+		MatToolbarModule,
+		MatIconModule,
+		MatButtonModule,
+		MatListModule,
+	],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'siab-web';
+	isVisible: boolean = false;
+	modules: IUrlOption[] = [
+		{ url: 'link', name: 'Estadisticas', icon: 'chevron_right' },
+		{ url: 'link', name: 'Procesos', icon: 'chevron_right' },
+		{ url: 'link', name: 'Mantenimientos', icon: 'chevron_right' },
+	];
 }
