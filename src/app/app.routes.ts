4468444cc3942d212985c64a815e84ad1296ec43
Adrian-DG <adrian96guilamo@gmail.com>
2025-02-04 00:41:53 -0400

    feat: add PROVEEDORES permission and update routes to include it in expected roles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,7 +11,12 @@ export const routes: Routes = [
 				(m) => m.proveedoresRoutes
 			),
 		canActivate: [authGuard, RolesGuard],
-		data: { expectedRoles: [AppPermissions.ADMINISTRADOR] },
+		data: {
+			expectedRoles: [
+				AppPermissions.ADMINISTRADOR,
+				AppPermissions.PROVEEDORES,
+			],
+		},
 	},
 	{
 		path: 'inventario',
