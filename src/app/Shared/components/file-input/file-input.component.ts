bc080133e80ed58f692f12e9a2cfba2f6404910b
Adrian Jose Diaz Guilamo <ajdiaz@ADRIANDIAZ>
2025-02-05 21:46:00 -0400

    feat: enhance file input component to support multiple file uploads and display file count

--- src/app/Shared/components/file-input/file-input.component.ts
+++ src/app/Shared/components/file-input/file-input.component.ts
@@ -5,7 +5,6 @@ import {
 	Input,
 	Output,
 } from '@angular/core';
-import { AbstractControl, ValidationErrors } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
@@ -20,23 +19,36 @@ import { MatIconModule } from '@angular/material/icon';
 export class FileInputComponent {
 	@Input() title!: string;
 	@Input() fileType!: string;
-	@Output('on-file-upluoded') fileEvent = new EventEmitter<string>();
-	file: File | null = null;
+	@Input('is-multiple') isMultiple: boolean = false;
+	@Output('on-file-upluoded') fileEvent = new EventEmitter<string[]>();
+	files: File[] | null = null;
+
+	get filesCount(): number {
+		return this.files ? this.files.length : 0;
+	}
 
 	onFileSelected(event: Event): void {
 		const input = event.target as HTMLInputElement;
 		if (input.files && input.files.length > 0) {
-			this.file = input.files[0];
-			const reader = new FileReader();
-			reader.onload = () => {
-				this.fileEvent.emit(reader.result as string);
-			};
-			reader.readAsDataURL(this.file);
+			this.files = Array.from(input.files);
+			const fileReaders = this.files.map((file) => {
+				return new Promise<string>((resolve) => {
+					const reader = new FileReader();
+					reader.onload = () => {
+						resolve(reader.result as string);
+					};
+					reader.readAsDataURL(file);
+				});
+			});
+
+			Promise.all(fileReaders).then((files) => {
+				this.fileEvent.emit(files);
+			});
 		}
 	}
 
 	removeFile(): void {
-		this.file = null;
-		this.fileEvent.emit('');
+		this.files = null;
+		this.fileEvent.emit([]);
 	}
 }
